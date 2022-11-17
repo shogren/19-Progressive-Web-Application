a64bfc8b9b308f33dbf54e711cc2a4f33934f994
jon-shogren <jon.shogren@mailchimp.com>
2022-11-16 20:00:32 -0500

    trying to fix npm install errors

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -26,7 +26,9 @@ export default class {
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      const allText = [];
+      const index = data.length - 1;
+      this.editor.setValue(data[index].content);
     });
 
     this.editor.on('change', () => {
