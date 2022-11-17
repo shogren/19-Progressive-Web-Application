cd89f651450d28661417382f1add88014be46c96
jon-shogren <jon.shogren@mailchimp.com>
2022-11-16 19:23:35 -0500

    install file fixed and working

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,36 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// logic for installing the PWA
+window.addEventListener('beforeinstallprompt', (event) => {
+    // prevent prompt from appearing on mobile
+    event.preventDefault();
+    console.log('👍', 'beforeinstallprompt', event);
+    // event to be triggered later
+    window.deferredPrompt = event;
+    // show install button
+    butInstall.classList.toggle('hidden', false);
+  });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    console.log('👍', 'butInstall-clicked');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // deferred prompt not available
+      return;
+    }
+    // show  install prompt
+    promptEvent.prompt();
+    const result = await promptEvent.userChoice;
+    console.log('👍', 'userChoice', result);
+    // reset deferred prompt
+    window.deferredPrompt = null;
+    // hide the install button
+    butInstall.classList.toggle('hidden', true);
+  });
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log("App installed!");
+    window.deferredPrompt = null;
+});
