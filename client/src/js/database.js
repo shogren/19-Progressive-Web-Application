5b1f331e548c9f112adae0a22d49dc1090c60b79
jon-shogren <jon.shogren@mailchimp.com>
2022-11-16 19:16:15 -0500

    database file working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,39 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// accept content and add it to the db
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ content: content }); // Or { id, content }
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// get all content from the db
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+// delete content from the db
+export const deleteDb = async (id) => {
+  console.log('DELETE from the database', id);
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.delete(id);
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
 
 initdb();
